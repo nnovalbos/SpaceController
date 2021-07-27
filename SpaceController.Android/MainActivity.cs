dd0547f3b7a71b4a49affb84e2d0b046b20e717a
Nicolás Novalbos <nicolas.novalbos@gmail.com>
2021-07-27 18:41:57 +0200

    Se cambia el color de la barra de estado en Android

--- SpaceController.Android/MainActivity.cs
+++ SpaceController.Android/MainActivity.cs
@@ -1,9 +1,8 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
+using Android.Runtime;
+using Xamarin.Forms;
 
 namespace SpaceController.Droid
 {
@@ -17,6 +16,8 @@ namespace SpaceController.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor(((Color)Xamarin.Forms.Application.Current.Resources["PrimaryColor"]).ToHex()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
